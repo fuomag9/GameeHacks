9b10660f156b16f808f4ad79e63fcaf5cf811b9e
fuomag9 <fuomag9@hotmail.it>
2017-04-25 23:09:58 +0200

    binded code to T keypress

--- script.js
+++ script.js
@@ -7,11 +7,14 @@
 // @match        https://www.gameeapp.com/game/*
 // @grant        none
 // ==/UserScript==
-
-(function() {
-    'use strict';
-    gameeUI.gameStart();
-    gameeUI.gamePause();
-    gameeUI.updateScore(prompt("Insert your desired score"));
-    gameeUI.gameOver();
+(function(){
+document.addEventListener('keydown', function(e) {
+  // pressed alt+g
+  if (e.keyCode == 84) {
+   gameeUI.gameStart();
+  gameeUI.gamePause();
+  gameeUI.updateScore(prompt("Insert your desired score"));
+  gameeUI.gameOver();
+  }
+}, false);
 })();
